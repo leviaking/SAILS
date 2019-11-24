ecfa06053b3cea67a680c33eaa035818cfe98166
Sails Corpus <sailscorpus@gmail.com>
2019-11-23 16:03:54 -0800

    Annotator 1 completed AB test for annotation feature weighting

--- scripts/ab_test_interface.py
+++ scripts/ab_test_interface.py
@@ -8,44 +8,54 @@ from shutil import copyfile
 
 
 ##GLOBAL STUFF
-inputfilename=sys.argv[1]
-INFA,INFB=inputfilename.split('-')
-INFNum=INFA[1:3]
-INFFeat=INFA[3:] ##e.g., 'TU_Gramm', 'U_Answer', etc.
-INFFeat2 = INFFeat.split('_')[1]
-INFItem=INFA.split('_')[0] ##e.g., 'I06TU', 'I11T'
-INFPIN=INFB.split('.')[0] ##e.g., '2039'
-# ipath = '../../all_items/figures_300_400/'
-ipath = '../../all_items/figures_200_266/'
+# # inputfilename=sys.argv[1]
+inputfilename = '/Users/leviking/Documents/dissertation/SAILS/weighting/I31T_Interp-2039abtest-LK.csv'
+# # INFA,INFB=inputfilename.split('-')
+# # INFNum=INFA[1:3]
+INFNum = '31'
+# # INFFeat=INFA[3:] ##e.g., 'TU_Gramm', 'U_Answer', etc.
+INFFeat = 'T_Interp'
+# # INFFeat2 = INFFeat.split('_')[1]
+# # INFItem=INFA.split('_')[0] ##e.g., 'I06TU', 'I11T'
+# # INFPIN=INFB.split('.')[0] ##e.g., '2039'
+ipath = '/Users/leviking/Documents/dissertation/SAILS/all_items/figures_200_266/'
 FeatNames = ['Gramm', 'Nativ', 'Interp', 'Core', 'Verif', 'Answer']
 
-def get_allresponses(csv_in):
-	allresponses = []
+def get_all_input_rows(csv_in):
+	allrows = []
 	with open(csv_in, 'rU') as masterfile:
 		masterreader = csv.reader(masterfile, dialect=csv.excel)
-		mheader1 = next(masterreader, None)
+		mheader = next(masterreader, None)
 		for mrow in masterreader:
-			mrow = mrow[0].strip(',')
-			#print mrow
-			allresponses.append(mrow)
-	return allresponses
+			# mrow = mrow[0].strip(',')
+			allrows.append(mrow)
+	return allrows
 
-RespStringList = get_allresponses(inputfilename)
-RespDenominator = len(RespStringList)
+def get_responses_from_rows(myrows):
+	resps = []
+	for mr in myrows:
+		resps.append(mr[13])
+	return resps
 
-header = [INFItem, INFItem+'_'+INFFeat2]
-with open('TEMP_'+inputfilename, 'w') as cfile:
+
+response_rows = get_all_input_rows(inputfilename)
+header = ['Response', 'Better', 'Worse', 'Same']
+RespStringList = get_responses_from_rows(response_rows)
+RespDenominator = len(RespStringList)/2  ## /2 to get number of pairs
+
+# # header = [INFItem, INFItem+'_'+INFFeat2]
+with open(inputfilename.split('.csv')[0]+'-TEMP.csv', 'w') as cfile:
 	cwriter = csv.writer(cfile, dialect=csv.excel)
 	cwriter.writerow(header)
 
-def write_type_csv(resp, anno):
-	row=[resp,anno]
-	with open('TEMP_'+inputfilename, 'a') as cfile:
+def write_type_csv(resp, win, lose, tie):
+	row=[resp,win, lose, tie]
+	with open(inputfilename.split('.csv')[0]+'-TEMP.csv', 'a') as cfile:
 		cwriter = csv.writer(cfile, dialect=csv.excel)
 		cwriter.writerow(row)
 		
 def EndOfFile():
-	InputName = 'TEMP_'+inputfilename
+	InputName = inputfilename.split('.csv')[0]+'-TEMP.csv'
 	FinishedName = 'anno'+InputName[4:]
 	try:
 		for fn in FeatNames:
@@ -58,7 +68,7 @@ def EndOfFile():
 		
 def back_button_edit():
 	oldrows=[]
-	with open('TEMP_'+inputfilename) as oldfile:
+	with open(inputfilename.split('.csv')[0]+'-TEMP.csv') as oldfile:
 		oldreader = csv.reader(oldfile, dialect=csv.excel)
 		for orow in oldreader:
 			oldrows.append(orow)
@@ -67,21 +77,20 @@ def back_button_edit():
 		newwriter = csv.writer(newfile, dialect=csv.excel)
 		for nrow in oldrows:
 			newwriter.writerow(nrow)
-	os.remove('TEMP_'+inputfilename)
-	os.rename('TEMP_GO_BACK_anno_'+inputfilename, 'TEMP_'+inputfilename)
-
+	os.remove(inputfilename.split('.csv')[0]+'-TEMP.csv')
+	os.rename('TEMP_GO_BACK_anno_'+inputfilename, inputfilename.split('.csv')[0]+'-TEMP.csv')
 
 FeatDict = {
-    'TU_Gramm': 'Does the response below meet the criteria for GRAMMATICALITY?',
-    'TU_Nativ': 'Does the response below meet the criteria for NATIVE-LIKENESS?',
-    'U_Answer': 'Based on the image and question, does the (untargeted) response meet the criteria for ANSWERHOOD?',
-    'T_Answer':'Based on the image and question, does the (targeted) response below meet the criteria for ANSWERHOOD?',
-    'U_Core':'Based on the image and question, does the (untargeted) response meet the criteria for CORE EVENT?',
-    'T_Core':'Based on the image and question, does the (targeted) response meet the criteria for CORE EVENT?',
-    'U_Interp':'Based on the image and question, does the (untargeted) response meet the criteria for INTERPRETABILITY?',
-    'T_Interp':'Does the (targeted) response meet the criteria for INTERPRETABILITY?',
-    'U_Verif':'Based on the image and question, does the (untargeted) response meet the criteria for VERIFIABILITY?',
-    'T_Verif':'Based on the image and question, does the (targeted) response meet the criteria for VERIFIABILITY?'}
+    'TU_Gramm': 'Which is the best response for this task?',
+    'TU_Nativ': 'Which is the best response for this task?',
+    'U_Answer': 'Which is the best response for this task?',
+    'T_Answer':'Which is the best response for this task?',
+    'U_Core':'Which is the best response for this task?',
+    'T_Core':'Which is the best response for this task?',
+    'U_Interp':'Which is the best response for this task?',
+    'T_Interp':'Which is the best response for this task?',
+    'U_Verif':'Which is the best response for this task?',
+    'T_Verif':'Which is the best response for this task?',}
 
 TargFeats = ['T_Answer', 'T_Core', 'T_Interp', 'T_Verif']
 NoImgTargFeats = ['T_Interp']
@@ -118,7 +127,9 @@ TargetedQs = ['What is the boy doing?',
 			  'What is the girl doing?',
 			  'What is the man doing?',
 			  'What is the woman doing?',
-			  'What is the woman doing?']
+			  'What is the woman doing?'
+			  'What is Randy doing?',
+			  'What is Randy doing?']
 CombinedQ = ''
 
 AP = FeatDict[INFFeat]
@@ -136,133 +147,157 @@ class App(Frame):
 		self.pack()
 		#self.createWidgets()
 	
-		self.CurrentRespNum = 0
+		self.LeftRespNum = 0
+		self.RightRespNum = 1
 		self.imagenum=INFNum
 		self.imagepath=ipath+'I'+self.imagenum+'.jpg'
 		
 		##QUIT BUTTON
 		self.QuitButton = Button(self, font=tf, width=20, highlightbackground="orange", text="Quit", command=self.quit)
 		self.QuitButton.grid(column=0,row=0,columnspan=1,sticky='W')
-			   
-		#QUESTION POINTER
-		if INFFeat not in CombinedFeats:
-			self.QPointer = Label(self, font=tf, text="Question: ", anchor='w')
-			self.QPointer.grid(column=0, row=2, columnspan=1, sticky='W')
-		else: pass
 			
-		#QUESTION LABEL
-		if INFFeat not in CombinedFeats:
-			self.CurrentQStringVar = StringVar()
-			self.CurrentQString = Label(self, height=1, width=65, font=tf, textvariable=self.CurrentQStringVar, anchor='w', wraplength=500, justify='left', bg='azure2')
-			self.CurrentQString.grid(column=0, pady=8, row=3,columnspan=1, sticky='EW')
-			self.CurrentQStringVar.set(PDTQ)
-		else:
-			pass
+		#QUESTION POINTER
+		# if INFFeat not in CombinedFeats:
+		# 	self.QPointer = Label(self, font=tf, text="Question: ", anchor='w')
+		# 	self.QPointer.grid(column=0, row=2, columnspan=1, sticky='W')
+		# else: pass
+			
+		# # #QUESTION LABEL
+		# # if INFFeat not in CombinedFeats:
+		# # 	self.LeftQStringVar = StringVar()
+		# # 	self.LeftQString = Label(self, height=1, width=65, font=tf, textvariable=self.LeftQStringVar, anchor='w', wraplength=500, justify='left', bg='azure2')
+		# # 	self.LeftQString.grid(column=0, pady=8, row=3,columnspan=1, sticky='EW')
+		# # 	self.LeftQStringVar.set(PDTQ)
+		# # else:
+		# # 	pass
 		
 		## ANNOTATION PROMPT
 		self.AnnotationPromptStringVar = StringVar()
 		#print AP
-		self.counter=' (Response #: '+str(self.CurrentRespNum+1)+'/'+str(RespDenominator)+'):'
+		self.counter=' (Pair #: '+str(self.RightRespNum)+'/'+str(RespDenominator)+'):'
 		self.AnnotationPrompt = Label(self, font=tf, text=AP+self.counter, width=65, anchor='w', wraplength=500, justify='left')
 		self.AnnotationPrompt.grid(column=0, pady=8, row=4, columnspan=1, sticky="W")
 		self.AnnotationPromptStringVar.set(self.AnnotationPrompt)
 
-		# ## IMAGE DISPLAY (working)
-		if INFFeat not in CombinedFeats and INFFeat not in NoImgTargFeats and INFFeat not in NoImgUntargFeats:
-			self.TempImage = ImageTk.PhotoImage(Image.open(self.imagepath))
-			self.CurrentImage = Label(self, image=self.TempImage)
-			self.CurrentImage.grid(row=1, column=0, sticky=EW)
-		else: pass
-	
+		# # ## IMAGE DISPLAY
+		# if INFFeat not in CombinedFeats and INFFeat not in NoImgTargFeats and INFFeat not in NoImgUntargFeats:
+		# 	self.TempImage = ImageTk.PhotoImage(Image.open(self.imagepath))
+		# 	self.LeftImage = Label(self, image=self.TempImage)
+		# 	self.LeftImage.grid(row=1, column=0, sticky=EW)
+		# else: pass
+
 		#RESPONSE LABEL
-		self.CurrentRespStringVar = StringVar()
-		self.CurrentRespString = Label(self, height=4, width=65, font=tf, textvariable=self.CurrentRespStringVar,
-								   anchor='w', wraplength=500, justify='left', bg="bisque")
-		self.CurrentRespString.grid(column=0,row=5,columnspan=1, sticky='EW')
-		self.CRS = RespStringList[self.CurrentRespNum]
-		self.CRS = self.CRS.rstrip(',')
-		self.CurrentRespStringVar.set(self.CRS)
+		self.LeftRespStringVar = StringVar()
+		# self.LeftRespString = Label(self, height=4, width=65, font=tf, textvariable=self.LeftRespStringVar,
+		# 						   anchor='w', wraplength=500, justify='left', bg="PaleTurquoise1")
+		# self.LeftRespString.grid(column=0,row=5,columnspan=1, sticky='EW')
+		self.LeftCRS = RespStringList[self.LeftRespNum]
+		self.LeftCRS = self.LeftCRS.rstrip(',')
+		#LEFT BUTTON
+		self.LeftButtonVar = StringVar()
+		self.LeftButton = Button(self, height=4, font=tf, textvariable=self.LeftRespStringVar,
+								anchor='w',justify=LEFT, highlightbackground="turquoise1", command=self.LeftKey)
+		self.LeftButton.grid(column=0,row=5,columnspan=1, sticky='EW', padx=20)
+		self.LeftRespStringVar.set(self.LeftCRS)
 		
-		#YES BUTTON
-		self.YesButtonVar = StringVar()
-		self.YesButton = Button(self, font=tf, width=20, highlightbackground="green", text="Yes", command=self.YesKey)
-		self.YesButton.grid(column=0,row=6, columnspan=1, padx=8, pady=8, sticky='W')
-	
-		#NO BUTTON
-		self.NoButtonVar = StringVar()
-		self.NoButton = Button(self, font=tf, width=20, highlightbackground='red', text="No", command=self.NoKey)
-		self.NoButton.grid(column=0,row=7, columnspan=1,  padx=8, pady=8, sticky='W')
+		self.RightRespStringVar = StringVar()
+		# self.RightRespString = Label(self, height=4, width=65, font=tf, textvariable=self.RightRespStringVar,
+		# 						   anchor='e', wraplength=500, justify='left', bg="LightGoldenrod1")
+		# self.RightRespString.grid(column=0,row=6,columnspan=1, sticky='EW')
+		self.RightCRS = RespStringList[self.RightRespNum]
+		self.RightCRS = self.RightCRS.rstrip(',')
+		#Right BUTTON
+		self.RightButtonVar = StringVar()
+		self.RightButton = Button(self, height=4, font=tf, textvariable=self.RightRespStringVar,
+								anchor='e', justify=LEFT, highlightbackground="LightGoldenrod1", command=self.RightKey)
+		self.RightButton.grid(column=0,row=6,columnspan=1, sticky='EW', padx=20)
+		self.RightRespStringVar.set(self.RightCRS)
 		
 		#MAYBE BUTTON
 		self.MaybeButtonVar = StringVar()
-		self.MaybeButton = Button(self, width=20, font=tf, text="Not sure", highlightbackground='purple', command=self.MaybeKey)
-		self.MaybeButton.grid(column=0, columnspan=1, row=6,  padx=8, pady=8, sticky='E')
+		self.MaybeButton = Button(self, width=20, font=tf, text="Same/Unsure", highlightbackground='pale green', command=self.MaybeKey)
+		self.MaybeButton.grid(column=0, columnspan=1, row=8,  padx=170, pady=8, sticky='EW')
 		
-		#BACK BUTTON
-		self.BackButtonVar = StringVar()
-		self.BackButton = Button(self, width=20, font=tf, text="Go back", highlightbackground='powder blue', command=self.BackKey)
-		self.BackButton.grid(column=0,row=7, padx=8, pady=8, sticky='E')	
-		
-	def BackKey(self, event=None):
-		self.CurrentRespNum -=1
-		self.CurrentRespStringVar.set(RespStringList[self.CurrentRespNum])
-		self.counter=' (Response #: '+str(self.CurrentRespNum+1)+'/'+str(RespDenominator)+'):'
-		self.AnnotationPrompt = Label(self, font=tf, text=AP+self.counter, width=65, anchor='w', wraplength=500, justify='left')
-		self.AnnotationPrompt.grid(column=0, pady=8, row=4, columnspan=1, sticky="W")
-		self.AnnotationPromptStringVar.set(self.AnnotationPrompt)
-		#back_button_reopen(self.CurrentRespNum)
-		back_button_edit()
-		
-	def YesKey(self, event=None):
-		self.annot = '1'
-		#print 'ANNOTATION = '+self.annot
-		write_type_csv(RespStringList[self.CurrentRespNum], self.annot)
-		self.CurrentRespNum +=1
-		#self.CRS = RespStringList[self.CurrentRespNum]
-		#self.CRS = self.CRS.rstrip(',')
-		#self.CurrentRespStringVar.set(self.CRS)
+	# 	#BACK BUTTON
+	# 	self.BackButtonVar = StringVar()
+	# 	self.BackButton = Button(self, width=20, font=tf, text="Go back", highlightbackground='powder blue', command=self.BackKey)
+	# 	self.BackButton.grid(column=0,row=8, padx=8, pady=8, sticky='E')	
+	# 	
+	# def BackKey(self, event=None):
+	# 	self.LeftRespNum -=2
+	# 	self.RightRespNum -=2
+	# 	self.LeftRespStringVar.set(RespStringList[self.LeftRespNum])
+	# 	self.RightRespStringVar.set(RespStringList[self.RightRespNum])
+	# 	self.counter=' (Pair #: '+str(self.LeftRespNum+1)+'/'+str(RespDenominator)+'):'
+	# 	self.AnnotationPrompt = Label(self, font=tf, text=AP+self.counter, width=65, anchor='w', wraplength=500, justify='left')
+	# 	self.AnnotationPrompt.grid(column=0, pady=8, row=4, columnspan=1, sticky="W")
+	# 	self.AnnotationPromptStringVar.set(self.AnnotationPrompt)
+	# 	back_button_edit()
+	 	
+	def LeftKey(self, event=None):
+		self.LWin = '1'
+		self.LLose = '0'
+		self.LTie = '0'
+		self.RWin = '0'
+		self.RLose = '1'
+		self.RTie = '0'
+		write_type_csv(RespStringList[self.LeftRespNum], self.LWin, self.LLose, self.LTie)
+		write_type_csv(RespStringList[self.RightRespNum], self.RWin, self.RLose, self.RTie)
+		self.LeftRespNum +=2
+		self.RightRespNum +=2
 		try:
-			self.CurrentRespStringVar.set(RespStringList[self.CurrentRespNum])
-
-			self.counter=' (Response #: '+str(self.CurrentRespNum+1)+'/'+str(RespDenominator)+'):'
+			self.LeftRespStringVar.set(RespStringList[self.LeftRespNum])
+			self.counter=' (Pair #: '+str((self.RightRespNum+1)/2)+'/'+str(RespDenominator)+'):'
 			self.AnnotationPrompt = Label(self, font=tf, text=AP+self.counter, width=65, anchor='w', wraplength=500, justify='left')
 			self.AnnotationPrompt.grid(column=0, pady=8, row=4, columnspan=1, sticky="W")
 			self.AnnotationPromptStringVar.set(self.AnnotationPrompt)
+			#
+			self.RightRespStringVar.set(RespStringList[self.RightRespNum])
 		except:
 			EndOfFile()
 			
 		
-	def NoKey(self, event=None):
-		self.annot = '0'
-		#print 'ANNOTATION = '+self.annot
-		write_type_csv(RespStringList[self.CurrentRespNum], self.annot)
-		self.CurrentRespNum +=1
-		#self.CRS = RespStringList[self.CurrentRespNum]
-		#self.CRS = self.CRS.rstrip(',')
-		#self.CurrentRespStringVar.set(self.CRS)
+	def RightKey(self, event=None):
+		self.LWin = '0'
+		self.LLose = '1'
+		self.LTie = '0'
+		self.RWin = '1'
+		self.RLose = '0'
+		self.RTie = '0'
+		write_type_csv(RespStringList[self.LeftRespNum], self.LWin, self.LLose, self.LTie)
+		write_type_csv(RespStringList[self.RightRespNum], self.RWin, self.RLose, self.RTie)
+		self.LeftRespNum +=2
+		self.RightRespNum +=2
 		try:
-			self.CurrentRespStringVar.set(RespStringList[self.CurrentRespNum])
-			self.counter=' (Response #: '+str(self.CurrentRespNum+1)+'/'+str(RespDenominator)+'):'
+			self.LeftRespStringVar.set(RespStringList[self.LeftRespNum])
+			self.counter=' (Pair #: '+str((self.RightRespNum+1)/2)+'/'+str(RespDenominator)+'):'
 			self.AnnotationPrompt = Label(self, font=tf, text=AP+self.counter, width=65, anchor='w', wraplength=500, justify='left')
 			self.AnnotationPrompt.grid(column=0, pady=8, row=4, columnspan=1, sticky="W")
 			self.AnnotationPromptStringVar.set(self.AnnotationPrompt)
+			#
+			self.RightRespStringVar.set(RespStringList[self.RightRespNum])
 		except:
 			EndOfFile()
 		
 	def MaybeKey(self, event=None):
-		self.annot = "5"
-		#print 'ANNOTATION ='+self.annot
-		write_type_csv(RespStringList[self.CurrentRespNum], self.annot)
-		self.CurrentRespNum+=1
-		#self.CRS = RespStringList[self.CurrentRespNum]
-		#self.CRS = self.CRS.rstrip(',')
-		#self.CurrentRespStringVar.set(self.CRS)
+		self.LWin = '0'
+		self.LLose = '0'
+		self.LTie = '1'
+		self.RWin = '0'
+		self.RLose = '0'
+		self.RTie = '1'
+		write_type_csv(RespStringList[self.LeftRespNum], self.LWin, self.LLose, self.LTie)
+		write_type_csv(RespStringList[self.RightRespNum], self.RWin, self.RLose, self.RTie)
+		self.LeftRespNum+=2
+		self.RightRespNum +=2
 		try:
-			self.CurrentRespStringVar.set(RespStringList[self.CurrentRespNum])
-			self.counter=' (Response #: '+str(self.CurrentRespNum+1)+'/'+str(RespDenominator)+'):'
+			self.LeftRespStringVar.set(RespStringList[self.LeftRespNum])
+			self.counter=' (Pair #: '+str((self.RightRespNum+1)/2)+'/'+str(RespDenominator)+'):'
 			self.AnnotationPrompt = Label(self, font=tf, text=AP+self.counter, width=65, anchor='w', wraplength=500, justify='left')
 			self.AnnotationPrompt.grid(column=0, pady=8, row=4, columnspan=1, sticky="W")
 			self.AnnotationPromptStringVar.set(self.AnnotationPrompt)
+			#
+			self.RightRespStringVar.set(RespStringList[self.RightRespNum])
 		except:
 			EndOfFile()
 
@@ -273,5 +308,4 @@ if __name__ == "__main__":
     root.geometry('600x800')
     app=App(master=root)
     app.mainloop()
-    #root.mainloop()
     root.destroy()
