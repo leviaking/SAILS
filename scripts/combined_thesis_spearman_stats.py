da9163ba51c43921fcc21ec175f7b8114189f6f2
leviaking <leviaking@gmail.com>
2021-04-01 16:54:19 -0700

    all ch 6 tables 100% confirmed, including term-norm experiments

---
+++ scripts/combined_thesis_spearman_stats.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+
+
+## 2021/04/01. Adapted from thesis_spearman_stats.py
+## operates on, e.g.: combined_spearman_N14-VS-N70.csv
+## or generally: combined_spearman_<XYZ>-VS-N70.csv , where XYZ is currently
+## either: F14 (Familiar), N14, or N50.
+## the point of this one is to get descriptive stats covering all 360 system
+## scores and descriptive stats covering all 120 BERT scores. I haven't needed
+## this yet but now I'm making tables for the weighted (termnorm) experiments
+## and those do need to scope over all 360 (or 120).
+
+
+import sys, math, csv
+from os import walk
+from scipy.stats import rankdata
+from scipy.stats import spearmanr
+import pandas
+pandas.options.display.float_format = '{:.10f}'.format
+
+
+def get_source_rows(tdf):
+	## header row:
+	"""Source	ldh_spear	ldh_p	xdh_spear	xdh_p	xdx_spear	xdx_p	BERT_spear	BERT_p"""
+	everything=[]
+	tdoc=open(tdf, 'r')
+	tdocreader=csv.reader(tdoc, dialect=csv.excel)
+	skipheader=next(tdocreader, None)
+	for row in tdocreader:
+		everything.append(row)
+	tdoc.close()
+	return skipheader, everything
+
+
+def write_output(rs, nm):
+	thisfile=open(nm, 'w')
+	thiswriter=csv.writer(thisfile, dialect=csv.excel)
+	for r in rs:
+		thiswriter.writerow(r)
+	thisfile.close()
+
+
+def get_spearman_dict(rws):
+	sources = []
+	sdict = {}
+	# ldh_sd = {}
+	# xdh_sd = {}
+	# xdx_sd = {}
+	for rw in rws:
+		srcn = rw[0]
+		ldh_k = srcn.replace("-r1-", "-r1-s_ldh-")
+		ldh_k = ldh_k.replace("-r2-", "-r2-s_ldh-")
+		sources.append(ldh_k)
+		xdh_k = srcn.replace("-r1-", "-r1-s_xdh-")
+		xdh_k = xdh_k.replace("-r2-", "-r2-s_xdh-")
+		sources.append(xdh_k)
+		xdx_k = srcn.replace("-r1-", "-r1-s_xdx-")
+		xdx_k = xdx_k.replace("-r2-", "-r2-s_xdx-")
+		sources.append(xdx_k)
+		bert_k = srcn.replace("-r1-", "-r1-bert-")
+		bert_k = bert_k.replace("-r2-", "-r2-bert-")
+		sources.append(bert_k)
+		sdict[ldh_k] = [float(rw[1]), float(rw[2])]
+		sdict[xdh_k] = [float(rw[3]), float(rw[4])]
+		sdict[xdx_k] = [float(rw[5]), float(rw[6])]
+		sdict[bert_k] = [float(rw[7]), float(rw[8])]
+	return sources, sdict
+
+## the original run:
+# my_exps = [['transitivity'], ['targeting'], ['primacy'], ['termrep']]
+## the termnorm run (covers ALL configurations (once for system, once for bert)):
+my_exps = [['termnorm']]
+## Use this param_dict for any crowdsourced training runs
+param_dict = {'transitivity': ["-In-", "-Tr-", "-Di-"], 'targeting': ['T-', 'U-'], 'familiarity': ["-gNSC-", "-gNSF-"], 'primacy': ["-r1-", "-r2-"], 'termrep': ['ldh', 'xdh', 'xdx'], 'termnorm': ["-s_", "-bert-"]}
+## Use this param_dict for any familiar training runs
+# param_dict = {'transitivity': ["-In-", "-Tr-", "-Di-"], 'targeting': ['T-', 'U-'], 'familiarity': ["-gNSC-", "-gNSF-"], 'primacy': ["-r2-"], 'termrep': ['ldh', 'xdh', 'xdx']}
+
+
+def from_experiment_get_stats_csvs(wd, exp, all_sources, sd, trr, tss):
+	tag = trr+"-VS-"+tss
+	for param in exp:
+		# print(all_sources)
+		# print("param: "+param)
+		settings = param_dict[param]
+		x_head = ['source', 'spearman', 'p_val']
+		param_header = []
+		param_stats = []
+		for setting in settings:
+			# print("setting a: "+setting)
+			setting_name = ''.join([i for i in setting if i.isalnum()])
+			# print("setting_name: "+setting_name)
+			ps_name = param+"_"+setting_name
+			ps_name = ps_name.replace("_s", "_sys")
+			ps_rows = []
+			for sr in all_sources:  ## e.g. 'I01T-gNSC-r1-In-N50-VS-N70'
+				# print("setting b: "+setting)
+				# print("sr: "+sr)
+				if setting in sr:
+					sr_spmn = sd[sr][0]
+					sr_pval = sd[sr][1]
+					ps_rows.append([sr, sr_spmn, sr_pval])
+					# t_rep_rows = query_termrep_dicts(sr, sd)
+					# for tr in t_rep_rows:
+					# 	ps_rows.append(tr)
+			ps_df = pandas.DataFrame(ps_rows)
+			# print(ps_df)
+			ps_sp_descriptive = ps_df[1].describe()
+			# print(ps_sp_descriptive)
+			ps_pv_descriptive = ps_df[2].describe()
+			param_header.append(ps_name)
+			param_stats.append(ps_sp_descriptive)
+			# print(ps_name)
+			## WEIGHTED version:
+			# ps_df.to_csv(wd+ps_name+'_spearman-'+tag+'-W.csv', encoding='utf-8',
+			# 			 index=False, header=x_head)
+			## UNWEIGHTED version:
+			ps_df.to_csv(wd+ps_name+'_spearman-'+tag+'.csv', encoding='utf-8',
+						 index=False, header=x_head)
+			## WEIGHTED version:
+			# ps_sp_descriptive.to_csv(wd+ps_name+'_spearman_stats-'+tag+'-W.csv',
+			# 						 encoding='utf-8', header=['spearman'])
+			## UNWEIGHTED version:
+			ps_sp_descriptive.to_csv(wd+ps_name+'_spearman_stats-'+tag+'.csv',
+									 encoding='utf-8', header=['spearman'])
+			## WEIGHTED version:
+			# ps_pv_descriptive.to_csv(wd+ps_name+'_sp_pval_stats-'+tag+'-W.csv', encoding='utf-8', header=['p_value'])
+			## UNWEIGHTED version:
+			ps_pv_descriptive.to_csv(wd+ps_name+'_sp_pval_stats-'+tag+'.csv', encoding='utf-8', header=['p_value'])
+		# param_col_zero = ['null', 'count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max']
+		p_descriptive = pandas.concat([pd for pd in param_stats], axis=1)
+		## WEIGHTED version
+		# p_descriptive.to_csv(wd+param+'_spearman_stats-'+tag+'-W.csv', encoding='utf-8', header=param_header)
+		## UNWEIGHTED version
+		p_descriptive.to_csv(wd+param+'_spearman_stats-'+tag+'.csv', encoding='utf-8', header=param_header)
+
+
+def main():
+	train_sample = "N50"
+	test_sample = "N70"
+	## WEIGHTED version
+	# working_dir=("/Users/leviking/Documents/dissertation/SAILS/stats/"+
+	# 			 train_sample+'-VS-'+test_sample+'-W/')
+	# comb_spearman_file = "combined_spearman_"+train_sample+"-VS-"+test_sample+"-W.csv"
+	## UNWEIGHTED version
+	working_dir=("/Users/leviking/Documents/dissertation/SAILS/stats/"+
+				 train_sample+'-VS-'+test_sample+'/')
+	comb_spearman_file = "combined_spearman_"+train_sample+"-VS-"+test_sample+".csv"
+	oldheader, comb_spearman_rows = get_source_rows(working_dir+comb_spearman_file)
+	# oldheader = ["Source", "ldh_uw_spear", "ldh_uw_p", "xdh_uw_spear", "xdh_uw_p", "xdx_uw_spear", "xdx_uw_p"]
+	comb_sources, s_dict = get_spearman_dict(comb_spearman_rows)  ## ['I01T-gNSC-r1-ldh-In-N50-VS-N70', ...], [ldh_sd, xdh_sd, xdx_sd]
+	for exp in my_exps:
+		from_experiment_get_stats_csvs(working_dir, exp, comb_sources, s_dict,
+									   train_sample, test_sample)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/get_bert_spearman_correlations-weighted_deps.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+
+## 2021/03/01. Branched from get_all_spearman_correlations.py
+## This script operates on the directory of test files in which each test
+## response in each test file already has a tf-idf cosine (TC) score;
+## This script reads in the (annotation based) GS ranking and the model based
+## ranking for each file and generates a Spearman correlation for the file.
+## (Actually, because each file combines LDH, XDH and XDX formats, there are
+## three scores per file.)
+## Each Spearman score is saved and written to a single csv.
+
+from os import walk
+from scipy.stats import spearmanr
+import pandas as pd
+
+
+def get_infile_names(somedir): 
+	docnames = []
+	for (dirpath, dirnames, filenames) in walk(somedir):
+		docnames.extend(filenames)
+		break
+	docnames = [dn for dn in docnames if ".csv" in dn]
+	docnames.sort()
+	return docnames
+
+
+def process_spearman(raw_bird):
+	# bird = pd.read_csv(bert_dir+raw_bird, index_col=0)
+	bird = pd.read_csv(bert_dir+raw_bird)
+	gold_ranks = bird["AnnoRank"]
+	bert_ranks = bird["BERT_rank"]
+	bsp, bpv = spearmanr(bert_ranks, gold_ranks)
+	return bsp, bpv
+
+
+def combine_csvs(bfull):
+	# oldie = pd.read_csv(statsdir+"all_spearman_N70.csv", index_col=0)
+	b = pd.DataFrame([bfull["BERT_spear"], bfull["BERT_p"]])
+	oldie = pd.read_csv(statsdir+"all_spearman_N70.csv")
+	newdf = pd.concat([oldie, b], axis=1)
+	print(newdf)
+	newdf.to_csv(statsdir+"/combined_spearman_"+test_sample+".csv", index=False)
+
+
+train_sample = "N50"
+test_sample = "N70"
+## For weighted_deps, we don't need to change the BERT scores location -- we use the same
+bert_dir=('/Users/leviking/Documents/dissertation/SAILS/test_data/scored/'+
+		  train_sample+'-VS-'+test_sample+'-BERT/')
+## Changed statsdir for the weighted_deps versions:
+statsdir=('/Users/leviking/Documents/dissertation/SAILS/stats/'+
+		  train_sample+'-VS-'+test_sample+'-W/')
+
+
+def main():
+	b_rank_docs = get_infile_names(bert_dir)
+	bspears = []
+	bpvals = []
+	blabels = []
+	for brd in b_rank_docs:
+		## transform the source label back to match the non-bert version
+		blab = brd.replace("-BERT.csv", "")
+		blab = blab.replace("-vs_r", "-r")
+		## We need to add "-W" here so the bert source label matches the "all_spearman...csv" label
+		blab = blab.replace("-"+test_sample, "-"+test_sample+"-W")
+		blabels.append(blab)
+		bspear, bpval = process_spearman(brd)
+		bspears.append(bspear)
+		bpvals.append(bpval)
+	bdict = {"Source": blabels, "BERT_spear": bspears, "BERT_p": bpvals}
+	bdf = pd.DataFrame(data=bdict)	
+	bdf.to_csv(statsdir+"bert_spearman_"+train_sample+"-VS-"+test_sample+
+			   "-W.csv", index=False)
+	
+	# b = pd.DataFrame([bfull["BERT_spear"], bfull["BERT_p"]])
+	oldie = pd.read_csv(statsdir+"all_spearman_"+
+						train_sample+"-VS-"+test_sample+"-W.csv")
+	# print(bdf)
+	# print(oldie)
+	newdf = pd.merge(oldie, bdf, on="Source")
+	print(newdf)
+	newdf.to_csv(statsdir+"/combined_spearman_"+train_sample+"-VS-"+
+				 test_sample+"-W.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
